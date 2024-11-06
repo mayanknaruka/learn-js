dc37fe6cf623325f9c07d6ee50cbc6b6b4e8dbdb
Mayank Naruka <mayanknaruka47@gmail.com>
2024-11-06 16:28:03 +0000

    Conversion Operations

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "mayank"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueinNumber = Number(score)
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true=1; false=0;
+
+let isloggedin =""
+
+let booleanloggedin = Boolean(isloggedin)
+console.log(booleanloggedin);
+
+// "" is false
+// "mayank" is true 
+
+let somenumber = 33;
+
+let stringnumber = String(somenumber)
+
+console.log(stringnumber)
+console.log(typeof stringnumber);
+
+
+
+
+
+
+
