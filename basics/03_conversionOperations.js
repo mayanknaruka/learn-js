10377e5b54337bf7e889400f1440b56d00ffe8c0
Mayank Naruka <mayanknaruka47@gmail.com>
2024-11-07 09:26:26 +0000

    operations

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -14,7 +14,7 @@ let valueinNumber = Number(score)
 let isloggedin =""
 
 let booleanloggedin = Boolean(isloggedin)
-console.log(booleanloggedin);
+// console.log(booleanloggedin);
 
 // "" is false
 // "mayank" is true 
@@ -23,8 +23,47 @@ let somenumber = 33;
 
 let stringnumber = String(somenumber)
 
-console.log(stringnumber)
-console.log(typeof stringnumber);
+// console.log(stringnumber)
+// console.log(typeof stringnumber);
+
+// ********************* Operations ****************
+
+let value = 3 
+let negValue = -value;
+console.log(negValue);
+
+// .console.log(2+2);
+// .console.log(2-2);
+// .console.log(2*2);
+// .console.log(2**2);
+// .console.log(2/2);
+// .console.log(2%2);
+
+let str1 = "hello";
+let str2 = " mayank";
+let str3 = str1 + str2;
+
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log( 3 + 4  * 5 % 6);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2, num3;
+
+num1= num2 = num3 = 2+2;   //don't use this 
+
+let game = 100;
+++game;
+console.log(game);
+
 
 
 
